535607d0ced64594f504e2b67acf0f0500ab1e15
yogeshgadhekar <70135882+yogeshgadhekar@users.noreply.github.com>
2020-09-07 17:51:00 +0530

    Add files via upload

---
+++ MigraionDemo/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3cc7ec68-df33-4238-8a32-03bdb91349e2")]
